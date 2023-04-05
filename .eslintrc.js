6ff187ed66432502ed93b866fc68d1005731bf4e
William Mosquera <wjmmk3@gmail.com>
2023-04-05 18:09:57 -0500

    settings Updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "env": {
-    "browser": true,
+    "node": true,
     "es6": true,
     "jest/globals": true
   },
@@ -19,13 +19,14 @@ module.exports = {
     "react", "jest"
   ],
   "rules": {
+    "no-console": "off", 
     "indent": [
       "error",
         2
-      ],
+      ], 
     "linebreak-style": [
       "error",
-      "unix"
+      "windows"
     ],
     "quotes": [
       "error",
